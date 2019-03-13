35ab6a7c7a581fa159c92c9f62516fbf27653d40
Bulut Oztemur (Garanti Teknoloji) <BulutOz@fw.garanti.com.tr>
2019-03-13 21:49:05 +0300

    Data

--- app/models.py
+++ app/models.py
@@ -4,11 +4,13 @@ from sqlalchemy.orm import sessionmaker, relationship, backref
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 
-engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
+
+engine = create_engine('postgres://ojrfzzowvvqupi:cd263cf0b985a723b7b087f9c1fdeeac4e225114d64a1871df98a69b03ff23fe@ec2-54-83-55-115.compute-1.amazonaws.com:5432/d4ndo4vffkmgg9')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
+Base.metadata.create_all(engine)
 
 class User(Base):
     __tablename__ = 'users'
@@ -17,18 +19,19 @@ class User(Base):
     username = Column(String)
     Password = Column(String(128))
 
+'''
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
+'''
 
 class Login(Base):
     __tablename__ = 'logins'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     user = relationship("User", backref="logins")
     date = Column(DateTime, default=datetime.datetime.utcnow)
 
@@ -63,24 +66,27 @@ class Book(Base):
 
     id = Column(Integer, primary_key=True)
     bookname = Column(String)
-    year = Column(Integer, )****
+    '''year = Column(Integer, )'''
     summary = Column(String)
-    image =
     authors = relationship("Author", secondary=books_authors_association)
     categories = relationship("Category", secondary=books_categories_association)
-
 
 
 class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
     commentinfo = Column(String)
+    book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     book = relationship("Book", backref="comments")
     user = relationship("User", backref="comments")
+
 
 class Point(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
-    pointval = Column(String)
+    pointval = Column(Integer)
+    book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     book = relationship("Book", backref="points")
     user = relationship("User", backref="points")
